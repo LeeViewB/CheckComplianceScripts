91146742e95c1908ede28f4463dbf3519cb5f9a2
LeeViewB <31350302+LeeViewB@users.noreply.github.com>
2023-07-30 11:44:11 +0300

    Initial commit

---
+++ Check-ComplianceMultipleApps/Check-ComplianceMultipleApps.ps1
@@ -0,0 +1,89 @@
+## make sure to enter the exact display name shown in Add Remove Programs.
+## While you can use wildcards to search for software, the exact display name discovered in appwiz.cpl will be used as the Setting Name for the json compliance check rule
+#[array]$applicationName = @("1Password","Discord","Test App") #example user apps installed in HKCU
+[array]$applicationName = @("Notepad++ (64-bit x64)","Google Chrome","Test App") #example machine wide apps installed in HKLM
+[bool]$userProfileApp = $false # Switch to true if you want the script to check for app info in HKCU instead of HKLM
+[bool]$isAppInstallCheckOnly = $true # if false, it will check only if the app exists. if true, it will only check if the app is installed or not
+
+# --------------------------------------
+# DO NOT EDIT THE LINES BELOW
+# --------------------------------------
+
+$appInfo = @()
+$appInfo = ForEach ($application in $applicationName) {
+    If(-not($isAppInstallCheckOnly -eq $true)){
+        [array]$myAppRegEntries = @()
+        If ($userProfileApp) {
+            # Search HKCU for a user-based app install            
+            # Testing the reg path for user based apps, if there are none, the path will not exist.
+            $myAppRegEntries += If (Test-Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall') { Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName, DisplayVersion }
+            $myAppRegEntries += If (Test-Path 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall') { Get-ItemProperty 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName, DisplayVersion }
+        } else {
+            # Search HKLM for a system-wide app install
+            $myAppRegEntries += Get-ItemProperty 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName, DisplayVersion        
+        }
+        # Flag to indicate if the application is installed
+        $appInstalled = $false
+
+        If ($myAppRegEntries) {
+            # Check if the app exists in $myAppRegEntries
+            Foreach ($myAppReg in $myAppRegEntries) {
+                if ($myAppReg.DisplayName -eq $application) {
+                    $appInstalled = $true
+                    [string]$displayName = $myAppReg.DisplayName
+                    [string]$displayVersion = $myAppReg.DisplayVersion
+                    break  # No need to check further once found
+                }                
+            }
+        }
+        if (-not $appInstalled) {
+            # App not installed, set the display name and version accordingly.
+            # If not setting this and the app is not installed, the version check would be null, causing the compliance check to error out.
+            # this way, if the software is not installed at all, forcing it to be listed as compliant.
+            $displayName = $application
+            $displayVersion = "999.0.0.0"
+        }
+        # Create a custom object and add it to the array
+        @{
+            $displayName = $displayVersion                    
+        }
+    }else{
+        # Checking only to see if the app is installed. Returning true/false. Not doing version check.
+        [array]$myAppRegEntries = @()
+        If ($userProfileApp) {
+            # Search HKCU for a user-based app install            
+            # Testing the reg path for user based apps, if there are none, the path will not exist.
+            $myAppRegEntries += If (Test-Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall') { Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName }
+            $myAppRegEntries += If (Test-Path 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall') { Get-ItemProperty 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName }
+        } else {
+            # Search HKLM for a system-wide app install
+            $myAppRegEntries += Get-ItemProperty 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName      
+        }
+
+        # Flag to indicate if the application is not installed
+        $appInstalled = $false
+        # Check if the app exists in $myAppRegEntries
+        foreach ($myAppReg in $myAppRegEntries) {
+            if ($myAppReg.DisplayName -eq $application) {
+                $appInstalled = $true
+                break  # No need to check further once found
+            }
+        }
+        
+        @{
+            $application = $appInstalled
+        }
+    }
+}
+
+
+# adding loop to convert the $appInfo array into a single custom object named $customObject
+# doing this because we want a single object with all the apps and versions listed as key-value pairs in the JSON output, instead of an array with separate objects for each app. Intune no likey that.
+$customObject = @{}
+foreach ($app in $appInfo) {
+    $customObject += $app
+}
+
+$hash = $customObject
+
+return $hash | ConvertTo-Json -Compress
---
+++ Check-ComplianceMultipleApps/readme.md
@@ -0,0 +1,70 @@
+# SYNOPSIS
+
+## Check-ComplianceMultipleApps.ps1
+The script will allow you to do 4 different compliance checks for one or multiple apps. Scroll down for Examples.
+- on row 3 type in the application names in the array based on the example mentioned in the script.
+- on row 4, you can specify if the script should check the HKCU or HKLM. 
+    - if set to $true - it will check HKCU. 
+    - if set to $false, it will check HKLM.
+- on row 5, you can specify if you want to do a version compliance check or just want to check if the app is installed.
+    - if set to $true, it will do a compliance check on the installed version of the software. As an example, your device should be Compliant if the Chrome version 116.1.1.1 or newer is installed. In this case, the script will look through the registry and check which version of Chrome is installed. 
+    - if set to $false, it will only do a compliance check to see whether the software is installed or not. As an example, your device should be Compliant if Google Chrome is not installed. In this case, the script will only check if Google Chrome is installed and will return **true** or **false**.
+
+# More Info
+[Create a compliance policy in Microsoft Intune](https://learn.microsoft.com/en-us/mem/intune/protect/create-compliance-policy)
+
+[Custom compliance discovery scripts for Microsoft Intune](https://learn.microsoft.com/en-us/mem/intune/protect/compliance-custom-script)
+
+[Custom compliance JSON files for Microsoft Intune](https://learn.microsoft.com/en-us/mem/intune/protect/compliance-custom-json)
+
+
+# Examples
+### 1. If you want to ensure that the installed **version** of the following software installed in **HKLM** is equal to or newer:
+- Google Chrome  = 116.0.5790.110
+- Notepad++ = 9.5.4
+- Test App  = 116.0.5790.110
+Make sure to:
+- on row 5, set 
+> $userProfileApp = $false
+- on row 6, set 
+> $isAppInstallCheckOnly = $false
+- use the the example json file for the custom detection rule: **complianceAppVersionHKLM.json**
+- if you want to check for other software, modify the array specified on row 4. Make sure to adjust the json file accordingly.
+
+### 2. If you want to ensure that the installed **version** of the following software installed in **HKCU** is equal to or newer:
+- 1Password  = 9.10.9
+- Discord = 1.0.9015
+- Test App = 1.0.9015
+Make sure to:
+- on row 5, set 
+> $userProfileApp = $true
+- on row 6, set 
+> $isAppInstallCheckOnly = $false
+- use the the example json file for the custom detection rule: **complianceAppVersionHKCU.json**
+- if you want to check for other software, modify the array specified on row 4. Make sure to adjust the json file accordingly.
+
+### 3. If you want to ensure that the software is NOT installed at all on the device for software that writes uninstall information in **HKLM**:
+- Google Chrome
+- Notepad++
+- Test App
+Make sure to:
+- on row 5, set 
+> $userProfileApp = $false
+- on row 6, set 
+> $isAppInstallCheckOnly = $true
+- use the the example json file for the custom detection rule: **complianceAppInstalledHKLM.json**
+- if you want to check for other software, modify the array specified on row 4. Make sure to adjust the json file accordingly.
+In this example, the device will be compliant if the software is NOT installed. The script will return True if the software is installed, and false if it isn't.
+
+### 4. If you want to ensure that the software is NOT installed at all on the device for software that writes uninstall information in **HKCU**:
+- 1Password
+- Discord
+- Test App
+Make sure to:
+- on row 5, set 
+> $userProfileApp = $true
+- on row 6, set 
+> $isAppInstallCheckOnly = $true
+- use the the example json file for the custom detection rule: **complianceAppInstalledHKCU.json**
+- if you want to check for other software, modify the array specified on row 4. Make sure to adjust the json file accordingly.
+In this example, the device will be compliant if the software is NOT installed. The script will return True if the software is installed, and false if it isn't.
