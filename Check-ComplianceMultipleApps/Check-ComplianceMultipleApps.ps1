11a131ab8684735076fb6e16001ca66c4a54cc0c
LeeViewB <31350302+LeeViewB@users.noreply.github.com>
2023-07-31 14:24:11 +0300

    Improved script. Looping through the registry for the list of installed apps only once.

--- Check-ComplianceMultipleApps/Check-ComplianceMultipleApps.ps1
+++ Check-ComplianceMultipleApps/Check-ComplianceMultipleApps.ps1
@@ -5,24 +5,23 @@
 [bool]$userProfileApp = $false # Switch to true if you want the script to check for app info in HKCU instead of HKLM
 [bool]$isAppInstallCheckOnly = $false # if false, it will check only if the app exists. if true, it will only check if the app is installed or not
 
-# --------------------------------------
+# -----------------------------------
 # DO NOT EDIT THE LINES BELOW
-# --------------------------------------
+# -----------------------------------
 
-$appInfo = @()
-[array]$myAppRegEntries = @()
-$appInfo = ForEach ($application in $applicationName) {
+If ($userProfileApp) {
+    # Search HKCU for a user-based app install            
+    # Testing the reg path for user based apps, if there are none, the path will not exist.
+    [array]$myAppRegEntries = If (Test-Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall') { Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction SilentlyContinue | Select-Object DisplayName, DisplayVersion }
+    [array]$myAppRegEntries += If (Test-Path 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall') { Get-ItemProperty 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Select-Object DisplayName, DisplayVersion }
+} else {
+    # Search HKLM for a system-wide app install
+    [array]$myAppRegEntries = Get-ItemProperty 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction SilentlyContinue | Select-Object DisplayName, DisplayVersion        
+}
+
+[array]$appInfo = ForEach ($application in $applicationName) {
     If(-not($isAppInstallCheckOnly -eq $true)){        
-        If ($userProfileApp) {
-            # Search HKCU for a user-based app install            
-            # Testing the reg path for user based apps, if there are none, the path will not exist.
-            $myAppRegEntries += If (Test-Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall') { Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName, DisplayVersion }
-            $myAppRegEntries += If (Test-Path 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall') { Get-ItemProperty 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName, DisplayVersion }
-        } else {
-            # Search HKLM for a system-wide app install
-            $myAppRegEntries += Get-ItemProperty 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName, DisplayVersion        
-        }
-        # Flag to indicate if the application is installed
+                # Flag to indicate if the application is installed
         $appInstalled = $false
 
         If ($myAppRegEntries) {
@@ -39,27 +38,15 @@ $appInfo = ForEach ($application in $applicationName) {
         if (-not $appInstalled) {
             # App not installed, set the display name and version accordingly.
             # If not setting this and the app is not installed, the version check would be null, causing the compliance check to error out.
-            # this way, if the software is not installed at all, forcing it to be listed as compliant.
+            # this way, if the software is not installed at all, the result for the software will be 0.0.0.0
             $displayName = $application
-            $displayVersion = "999.0.0.0"
+            $displayVersion = "0.0.0.0"
         }
         # Create a custom object and add it to the array
         @{
             $displayName = $displayVersion                    
         }
     }else{
-        # Checking only to see if the app is installed. Returning true/false. Not doing version check.
-        [array]$myAppRegEntries = @()
-        If ($userProfileApp) {
-            # Search HKCU for a user-based app install            
-            # Testing the reg path for user based apps, if there are none, the path will not exist.
-            $myAppRegEntries += If (Test-Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall') { Get-ItemProperty 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName }
-            $myAppRegEntries += If (Test-Path 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall') { Get-ItemProperty 'HKCU:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName }
-        } else {
-            # Search HKLM for a system-wide app install
-            $myAppRegEntries += Get-ItemProperty 'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -like $application } | Select-Object DisplayName      
-        }
-
         # Flag to indicate if the application is not installed
         $appInstalled = $false
         # Check if the app exists in $myAppRegEntries
